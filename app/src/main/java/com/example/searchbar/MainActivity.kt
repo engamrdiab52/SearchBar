8e3aca677db3d09a98463770faaf38246b5926f9
engam <engamrdiab52@gmail.com>
2021-09-26 10:00:24 +0200

    initial commit

---
+++ app/src/main/java/com/example/searchbar/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.example.searchbar
+
+import android.app.SearchManager
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.widget.Toast
+import androidx.appcompat.widget.SearchView
+
+class MainActivity : AppCompatActivity() {
+    companion object{
+        const val TAG = "MainActivity"
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        val searchItem = menu?.findItem(R.id.search)
+        val searchView = searchItem?.actionView as SearchView
+
+        val manager = getSystemService(Context.SEARCH_SERVICE) as SearchManager
+
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                searchView.clearFocus()
+                searchView.setQuery("", false)
+                searchItem.collapseActionView()
+                Toast.makeText(this@MainActivity, "Looking for $query", Toast.LENGTH_SHORT).show()
+                return true
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                newText?.let { Log.d(TAG, it) }
+                return true
+            }
+
+        })
+        return true
+    }
+}
